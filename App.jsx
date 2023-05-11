e1bd985460dbe4770e8ec1d21b300827fd4553a7
Rachael Hindley <rachael.hindley52@gmail.com>
2023-05-11 15:19:06 +0100

    lab08 attempt

--- App.jsx
+++ App.jsx
@@ -8,6 +8,7 @@ function App() {
     const [location, setLocation] = useState({});
     const [mapImg, setMapImg] = useState("");
     const [apiError, setApiError] = useState("");
+    const [weather, setWeather] = useState("");
 
     function handleChange(event) {
         console.log(event.target.value);
@@ -21,12 +22,27 @@ function App() {
             const res = await axios.get(API);
             setLocation(res.data[0]);
             handleMap(res.data[0]);
+            getWeather(res.data[0]);
             setApiError("");
         } catch (error) {
             console.log(error);
             setApiError(error.message);
             setLocation({});
+            setWeather([]);
             setMapImg("");
+        }
+    }
+
+    async function getWeather(data) {
+        try {
+            // const API = `https://localhost:8081/weather?searchQuery=${searchQuery}&lat=${data.lat}&lon=${data.lon}`;
+            const API = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${data.lat},${data.lon}`;
+            const res = await axios.get(API);
+            console.log(res.data);
+            setWeather(res.data);
+        } catch (error) {
+            console.log(error);
+            setWeather([]);
         }
     }
 
@@ -47,6 +63,13 @@ function App() {
                 longitude: {location.lon}, latitude: {location.lat}
             </p>
             {mapImg && <img src="{mapImg}" alt="map" />}
+            {weather.map((day) => {
+                return (
+                    <p>
+                        {day.date} - {day.description}
+                    </p>
+                );
+            })}
         </div>
     );
 }
--- server.jsx
+++ server.jsx
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 const app = express();
 app.use(cors());
+const axios = require("axios");
 
 const data = require("./data/weather.json");
 
@@ -11,19 +12,34 @@ app.get("/", (request, response) => {
     response.json("This is the root route.");
 });
 
-app.get("/weather", (request, response) => {
-    const { lat, lon, searchQuery } = request.query;
+app.get("/weather", async (request, response) => {
+    // const { lat, lon, searchQuery } = request.query;
     // find will stop looking with the first it finds but filter finds all of the options and gives it in an array.
-    const city = data.find((item) => {
-        return item.city_name === searchQuery;
+    const API = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${data.lat},${data.lon}`;
+    const res = await axios.get(API);
+    const forecasts = res.data.results.map((forecast) => {
+        return {
+            city_name: forecast.city_name,
+            latitude: forecast.lat,
+            longitude: forecast.lon,
+        };
     });
-    const forecasts = [];
-    city.data.forEach((day) => {
-        const fc = { date: day.valid_date, description: day.weather.description };
-        forecasts.push(fc);
-    });
+    // const forecasts = [];
+    // try {
+    //     const city = data.find((item) => {
+    //         return item.city_name === searchQuery;
+    //     });
+
+    //     city.data.forEach((day) => {
+    //         const fc = { date: day.valid_date, description: day.weather.description };
+    //         forecasts.push(fc);
+    //     });
+    // } catch (error) {
+    //     console.log(error);
+    // }
 
     response.json(forecasts);
+    console.log(forecasts);
 });
 
 app.listen(PORT, () => console.log(`App is running on PORT ${PORT}`));
