05097fae608847cd7560aafa37cd3f71eaca3cd7
Rachael Hindley <rachael.hindley52@gmail.com>
2023-05-11 09:06:15 +0100

    lab 07 code copied main client branch

---
+++ App.jsx
@@ -0,0 +1,52 @@
+import "./Reset.css";
+import "./App.css";
+import { useState } from "react";
+import axios from "axios";
+
+function App() {
+    const [searchQuery, setSearchQuery] = useState("");
+    const [location, setLocation] = useState({});
+    const [mapImg, setMapImg] = useState("");
+    const [apiError, setApiError] = useState("");
+
+    function handleChange(event) {
+        console.log(event.target.value);
+        setSearchQuery(event.target.value);
+    }
+
+    async function getLocation() {
+        try {
+            const API = `https://eu1.locationiq.com/v1/search?key=${process.env.REACT_APP_API_KEY}&q=${searchQuery}&format=json`;
+            console.log(API);
+            const res = await axios.get(API);
+            setLocation(res.data[0]);
+            handleMap(res.data[0]);
+            setApiError("");
+        } catch (error) {
+            console.log(error);
+            setApiError(error.message);
+            setLocation({});
+            setMapImg("");
+        }
+    }
+
+    function handleMap(data) {
+        const API = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_KEY}&center=${data.lat},${data.lon}&zoom=12`;
+        setMapImg(API);
+    }
+
+    return (
+        <div className="App">
+            <h1>Location IQ API</h1>
+            <p></p>
+            <input type="text" onChange={handleChange} placeholder="enter location" />
+            <button onClick={getLocation}>Explore</button>
+            <p>{apiError}</p>
+            <h2>{location.display_name}</h2>
+            <p>
+                longitude: {location.lon}, latitude: {location.lat}
+            </p>
+            {mapImg && <img src="{mapImg}" alt="map" />}
+        </div>
+    );
+}
