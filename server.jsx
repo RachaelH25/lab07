1626d349ebb62ac13080aa8bfd65c359f01447cc
Rachael Hindley <rachael.hindley52@gmail.com>
2023-05-12 13:52:46 +0100

    serverjsx update. lab08 card1.

--- server.jsx
+++ server.jsx
@@ -13,33 +13,42 @@ app.get("/", (request, response) => {
 });
 
 app.get("/weather", async (request, response) => {
-    // const { lat, lon, searchQuery } = request.query;
+    const { lat, lon, searchQuery } = request.query;
     // find will stop looking with the first it finds but filter finds all of the options and gives it in an array.
-    const API = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${data.lat},${data.lon}`;
-    const res = await axios.get(API);
-    const forecasts = res.data.results.map((forecast) => {
-        return {
-            city_name: forecast.city_name,
-            latitude: forecast.lat,
-            longitude: forecast.lon,
-        };
-    });
-    // const forecasts = [];
-    // try {
-    //     const city = data.find((item) => {
-    //         return item.city_name === searchQuery;
-    //     });
+    const forecasts = [];
+    try {
+        const API = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${data.lat},${data.lon}`;
+        const res = await axios.get(API);
 
-    //     city.data.forEach((day) => {
-    //         const fc = { date: day.valid_date, description: day.weather.description };
-    //         forecasts.push(fc);
-    //     });
-    // } catch (error) {
-    //     console.log(error);
-    // }
-
-    response.json(forecasts);
-    console.log(forecasts);
+        res.data.data.forEach((day) => {
+            const fc = { date: day.valid_date, description: day.weather.description };
+            forecasts.push(fc);
+        });
+        response.status(200).json(forecasts);
+    } catch (error) {
+        console.log(error);
+        response.status(404).json("Location not found.");
+    }
 });
+// const forecasts = res.data.results.map((forecast) => {
+//     return {
+//         city_name: forecast.city_name,
+//         latitude: forecast.lat,
+//         longitude: forecast.lon,
+//     };
+// });
+// const forecasts = [];
+// try {
+//     const city = data.find((item) => {
+//         return item.city_name === searchQuery;
+//     });
+
+//     city.data.forEach((day) => {
+//         const fc = { date: day.valid_date, description: day.weather.description };
+//         forecasts.push(fc);
+//     });
+// } catch (error) {
+//     console.log(error);
+// }
 
 app.listen(PORT, () => console.log(`App is running on PORT ${PORT}`));
